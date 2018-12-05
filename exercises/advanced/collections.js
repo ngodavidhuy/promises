1fe27bb8d9548a2b2675ee1aad0f0d4030b1096b
ngodavidhuy <ngodavidhuy@gmail.com>
2018-12-04 19:54:57 -0800

    Implements advanced collections.js

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,19 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 
-
-var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+var combineFirstLineOfManyFiles = function (filePaths, writePath) {
+  // TODO
+  let promiseArray = filePaths.map(file => {
+    return fs.readFileAsync(file, 'utf8').then(text => {
+      return text.split('\n')[0];
+    });
+  });
+  return Promise.all(promiseArray).then(newArr => {
+    fs.writeFileAsync(writePath, newArr.join('\n'), 'utf8');
+  });
 };
 
 // Export these functions so we can unit test them
--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-describe('Bare Minimum Requirements', function() {
+describe('Bare Minimum Requirements', function () {
   require('./bare_minimum/callbackReview.test.js');
   require('./bare_minimum/promiseConstructor.test.js');
   require('./bare_minimum/promisification.test.js');
   require('./bare_minimum/basicChaining.test.js');
 });
 
-xdescribe('Advanced Content', function() {
- 
+describe('Advanced Content', function () {
+
   require('./advanced/collections.test.js');
   require('./advanced/advancedChaining.test.js');
   require('./advanced/PromiseLib.test.js');
